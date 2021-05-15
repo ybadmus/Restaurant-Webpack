64cfa9bb0d967b9035477ac17da616c082eebb90
ybadmus <yusif.badmus@gmail.com>
2021-05-15 18:05:11 +0100

    create header module, add to index.js to render dynamicaly

--- src/header.js
+++ src/header.js
@@ -1,5 +1,7 @@
 const header = () => {
-    const navbar_class = ['navbar', 'navbar-expand-lg', 'navbar-dark', 'bg-dark']
+    const navbar_class = ['navbar', 'navbar-expand-lg', 'navbar-dark', 'bg-dark'];
+    const navbar_collapse = ['collapse', 'navbar-collapse']
+
     const nav = document.createElement('nav');
     nav.classList.add(...navbar_class);
 
@@ -17,13 +19,37 @@ const header = () => {
     button.classList = 'navbar-toggler';
     button.attributes.type = 'button';
 
-    container_fluid.appendChild(button);
-
     const span = document.createElement('span');
     span.classList.add('navbar-toggler-icon');
+    button.appendChild(span);
 
-    container_fluid.appendChild(span);
+    container_fluid.appendChild(button);
 
+    const collapse = document.createElement('div');
+    collapse.classList.add(...navbar_collapse);
+    collapse.id = 'navbarNavAltMarkup';
+
+    const navbar_nav = document.createElement('div');
+    navbar_nav.classList.add('navbar-nav');
+
+    const links = [{ name: 'Home', href: '#', active: true }, { name: 'Menu', href: '#', active: false }, { name: 'Contact', href: '#', active: false }];
+
+    for (let i = 0; i < links.length; i++) {
+
+        const nav_link = document.createElement('a');
+        nav_link.classList.add('nav-link');
+        if (links[i].active) nav_link.classList.add('active');
+        nav_link.attributes.href = links[i].href;
+        nav_link.textContent = links[i].name;
+
+        navbar_nav.appendChild(nav_link);
+    };
+
+    collapse.appendChild(navbar_nav);
+    container_fluid.appendChild(collapse);
+    nav.appendChild(container_fluid);
+
+    return nav;
 }
 
 export { header };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { footer } from './footer';
+import { header } from './header';
 import './style.css';
 
-function index() {
-    return footer();
+function render() {
+    const content = document.querySelector('#content');
+    content.appendChild(header());
+    content.appendChild(footer());
+    return content;
 };
 
-const content = document.querySelector('#content');
-content.appendChild(index());
+render();
