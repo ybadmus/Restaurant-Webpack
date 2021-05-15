90f2984bfda3a7a304eacf570acd22614e5aacdf
ybadmus <yusif.badmus@gmail.com>
2021-05-16 00:30:29 +0100

    add home.js module for the home page of the app

---
+++ src/home.js
@@ -0,0 +1,36 @@
+const home = () => {
+
+    const app = document.createElement('div');
+    app.classList.add('app');
+
+    const restaurantName = document.createElement('h2');
+    restaurantName.className = 'restaurant-name';
+    restaurantName.textContent = 'RESTAURANT';
+
+    app.appendChild(restaurantName);
+
+    const title = document.createElement('h5');
+    title.className = 'restaurant-title';
+    title.textContent = 'Title Of Restaurant';
+
+    app.appendChild(title);
+
+    const listOfActions = ['OPENING HOURS', 'MAKE A RESERVATION', 'PRIVATE DINING', 'EVENTS', 'LOCATION']
+
+    const ul = document.createElement('ul');
+    ul.className = 'actions-list';
+
+    for (let i = 0; i < listOfActions.length; i++) {
+        const li = document.createElement('li');
+        li.className = 'action';
+        li.textContent = listOfActions[i];
+        ul.appendChild(li);
+    }
+
+    app.appendChild(ul);
+
+    return app;
+
+};
+
+export { home };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { footer } from './footer';
 import { header } from './header';
+import { home } from './home';
 import './style.css';
 
 function render() {
     const content = document.querySelector('#content');
     content.appendChild(header());
+    content.appendChild(home());
     content.appendChild(footer());
     return content;
 };
