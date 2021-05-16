8953ef177b259874c836ec167793dbd05ddb7ffe
ybadmus <yusif.badmus@gmail.com>
2021-05-16 01:10:45 +0100

    ad about us js file for about us page of app

---
+++ src/about.js
@@ -0,0 +1,33 @@
+  const about = () => {
+
+      const app = document.createElement('div');
+      const classes = ['about', 'container'];
+      app.classList.add(...classes);
+
+      const title = document.createElement('h1');
+      title.textContent = 'About';
+
+      app.appendChild(title);
+
+      const about = document.createElement('div');
+      about.className = 'about';
+
+      const aboutText = [
+          'We provide our customers with luxurious dining and an overall unforgettable experience',
+          'Steakhouse is currently situated in over 15 cities worldwide',
+          'Book a reservation today',
+      ];
+
+      for (let i = 0; i < aboutText.length; i++) {
+          const paragraph = document.createElement('p');
+          paragraph.textContent = aboutText[i];
+          about.appendChild(paragraph);
+      };
+
+      app.appendChild(about);
+
+      return app;
+
+  };
+
+  export { about };
