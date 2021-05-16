ff3f988e0b36eecf9bb71621958e7d4fb0abae8a
ybadmus <yusif.badmus@gmail.com>
2021-05-17 00:31:56 +0100

    fix all styling errors

--- src/index.js
+++ src/index.js
@@ -1,48 +1,51 @@
-import footer from './footer';
-import header from './header';
-import home from './home';
-import about from './about';
-import contact from './contact';
-import menu from './menu';
+import footer from './footer.js';
+import header from './header.js';
+import home from './home.js';
+import about from './about.js';
+import contact from './contact.js';
+import menu from './menu.js';
 import './style.css';
 
 const render = (item) => {
-    document.getElementById('content').innerHTML = '';
+  document.getElementById('content').innerHTML = '';
 
-    const content = document.querySelector('#content');
-    content.appendChild(header());
+  const content = document.querySelector('#content');
+  content.appendChild(header());
 
-    switch (item) {
-        case 'home':
-            content.appendChild(home());
-            break;
-        case 'menu':
-            content.appendChild(menu());
-            break;
-        case 'about':
-            content.appendChild(about());
-            break;
-        case 'contact':
-            content.appendChild(contact());
-            break;
-        default:
-            content.appendChild(home());
-    }
+  switch (item) {
+    case 'home':
+      content.appendChild(home());
+      break;
+    case 'menu':
+      content.appendChild(menu());
+      break;
+    case 'about':
+      content.appendChild(about());
+      break;
+    case 'contact':
+      content.appendChild(contact());
+      break;
+    default:
+      content.appendChild(home());
+  }
 
-    content.appendChild(footer());
-    return content;
+  content.appendChild(footer());
+  return content;
 };
 
 render();
 
 document.addEventListener('click', (event) => {
-    if (event.target.className == 'nav-link') {
-        render(event.target.id);
+  if (event.target.className === 'nav-link') {
+    render(event.target.id);
 
-        const nav_links = document.querySelectorAll('.nav-link');
-        nav_links.forEach((navlink) => navlink.className = 'nav-link');
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach((navlink) => {
+      navlink.className = 'nav-link';
+      return navlink;
+    });
 
-        const nav_link = document.querySelector(`#${event.target.id}`);
-        nav_link.classList.add('active');
-    }
+    const navLink = document.querySelector(`#${event.target.id}`);
+    navLink.classList.add('active');
+  }
 });
